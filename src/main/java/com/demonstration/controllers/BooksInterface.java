12f2600519670eb7464aab55c8ff7c308eb4e9a0
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-09-14 15:46:10 +0100

    Update BooksInterface.java

--- src/main/java/com/demonstration/controllers/BooksInterface.java
+++ src/main/java/com/demonstration/controllers/BooksInterface.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import com.demonstration.entities.Books;
 
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+
 
 public interface BooksInterface {
-public Flux<Books> getAllBooks();
-public Mono<Books> getOne(long id);
+public List<Books> getAllBooks();
+public Books getOne(long id);
 public void deleteBook(long id);
-public Mono<Books> addOneBook(Books books);
+public Books addOneBook(Books books);
 }
